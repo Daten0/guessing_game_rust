0801a8801f387f694e5ede36ffa9c6d014bcc44b
DatenMints <febrimicrone@gmail.com>
2026-05-27 17:01:10 +0800

    add ; implement random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::{RngExt};
 
 fn main() {
     println!("Please input your guess number");
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    println!("The secret number is : {}", secret_number);
 
     let mut guess = String::new();
 
