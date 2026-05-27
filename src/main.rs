1bbca7d54f5c645d02f138cfe13608cd73459203
DatenMints <febrimicrone@gmail.com>
2026-05-27 17:54:12 +0800

    add ; Pattern Matching of the random numbers with our numbers input, also wrapping the core programs inside a logic for better interactive of a programs, and determined to stop the program if the guessed number is right on the number generator

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,47 @@
 use std::io;
 use rand::{RngExt};
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Please input your guess number");
-
     let secret_number = rand::rng().random_range(1..=100);
 
-    println!("The secret number is : {}", secret_number);
+    loop {
+        println!("Please input your guess number");
 
-    let mut guess = String::new();
+        // println!("The secret number is : {}", secret_number);
 
-    io::stdin().
-        read_line(&mut guess).
-        expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed : {}", guess);
+        io::stdin().
+            read_line(&mut guess).
+            expect("Failed to read line");
 
+        // let guess = guess.trim().parse::<u32>().expect("Type a freakin number dumbass!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("This is not a freakin number dumbass!");
+                continue 
+            },
+        };
+
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("Your guessed is right");
+                println!("You guessed : {}", guess);
+                break;
+            },
+            Ordering::Greater => {
+                println!("Your guessed number too big");
+                println!("You guessed : {}", guess);
+                println!("Guessed again!");
+            }
+            Ordering::Less => {
+                println!("Your guessed number is lessed than what it generated");
+                println!("You guessed : {}", guess);
+                println!("Guessed again!");
+            }
+        }   
+    }
 }
